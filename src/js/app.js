863446c2d62a22b4561776f6a5dcb24b1a5c1568
fongreecss <fongreecss@gmail.com>
2020-10-06 19:19:29 +0200

    chart section  mobile

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,14 @@ import Parallax from "./components/parallax";
 import InViewport from './components/inviewport';
 import Pikaday from "pikaday";
 import OrderForm from "./components/order-form";
+import TinySlider from "./components/tiny-slider";
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('body').classList.add('dom-ready');
   document.querySelector('html').classList.remove('no-js');
   //new CookieNotice();
   //new Navigation();
+  new TinySlider();
   new Parallax();
   new OrderForm();
   var picker = new Pikaday({ 
--- src/js/components/tiny-slider.js
+++ src/js/components/tiny-slider.js
@@ -6,13 +6,20 @@ export default class TinySlider {
   constructor() {
     document.querySelectorAll(".tns-slider").forEach(el => {
       var slides = el.dataset.tnsSlides ? el.dataset.tnsSlides * 1 : 1;
+      var autoplaySlides = el.hasAttribute('data-autoplay') ? true : false;
+      var loopSlides = el.hasAttribute('data-loop') ? true : false;
+      var autoWidthSlide = el.hasAttribute('data-auto-width') ? true : false;
       console.log(slides);
       tns({
         container: ".tns-slider",
-        autoplay: true,
-        autoWidth: true,
+        autoplay: autoplaySlides,
+        autoWidth: autoWidthSlide,
         items: slides,
-        lazyload: true
+        lazyload: true,
+        controls: false,
+        autoplayButton: false,
+        nav: false,
+        loop: loopSlides,
       });
     });
   }
