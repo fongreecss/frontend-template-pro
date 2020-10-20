523d2f3b3cedeaf74d39f3f1fd21ae8c3171d910
fongreecss <fongreecss@gmail.com>
2020-10-20 18:41:49 +0200

    slider on page 1 consult

--- src/js/components/tiny-slider.js
+++ src/js/components/tiny-slider.js
@@ -5,23 +5,28 @@ import { tns } from "tiny-slider/src/tiny-slider";
 export default class TinySlider {
 
   addClassName(slider, sel, info) {
-     var sel1= '[id*="-item' + info.index + '"]';
+      var sel1= '[id*="-item' + info.index + '"]';
       var sel2 = sel + " " + sel1;
       var sel3 = sel + " " + '[id*="-item"]';
-      document.querySelectorAll(sel3).forEach(el => {
-        el.classList.remove('tns-s-active');
-      });
-      document.querySelector(sel2).classList.add('tns-s-active');
+      try {
+        document.querySelectorAll(sel3).forEach(el => {
+          el.classList.remove('tns-s-active');
+        });
+        document.querySelector(sel2).classList.add('tns-s-active');
+      } catch(err) {
+
+      }
+      
   }
 
   constructor() {
-    window.tnsSliders = [];
-    document.querySelectorAll(".tns-slider").forEach(el => {
+      document.querySelectorAll(".tns-slider").forEach(el => {
       
+      var sliders= typeof(el.id) == "undefined" ? ".tns-slider" : "#" + el.id; 
       var sel= typeof(el.id) == "undefined" ? ".tns-slider" : "#" + el.id; 
       var sliderId = typeof(el.id) == "undefined" ? "" :  el.id; 
       var tnsSpeed = el.hasAttribute('data-tns-speed') ? +el.dataset.sliderSpeed : 300;
-      var slides = el.dataset.tnsSlides ? el.dataset.tnsSlides * 1 : 1;
+      var tnsItems = el.dataset.tnsItems ? el.dataset.tnsItems * 1 : 1;
       var tnsAutoplay = el.hasAttribute('data-tns-autoplay') ? true : false;
       var tnsLoop = el.hasAttribute('data-tns-loop') ? true : false;
       var tnsAutowidth = el.hasAttribute('data-tns-autowidth') ? true : false;
@@ -29,9 +34,10 @@ export default class TinySlider {
       var tnsMouseDrag = el.hasAttribute('data-tns-mouse-drag') ? true : false;
       var tnsPrevButton = el.dataset.tnsPrevButton ? document.querySelector(el.dataset.tnsPrevButton) : document.querySelector(sel + " .tns-prev-button");
       var tnsNextButton = el.dataset.tnsNextButton ? document.querySelector(el.dataset.tnsNextButton) : document.querySelector(sel + " .tns-next-button");
-      
+      var tnsMode = el.hasAttribute('data-tns-gallery') ? "gallery" : "carousel";
       var options = {
         container: sel,
+        mode : tnsMode,
         autoplay: tnsAutoplay,
         autoWidth: tnsAutowidth,
         swipeAngle : false,
@@ -48,6 +54,10 @@ export default class TinySlider {
           this.addClassName(this, sel, info);
         },
       }
+      if(tnsItems) {
+        options = { ...options, items : tnsItems };
+      }
+      
       var temp = tns(options);
       temp.events.on("indexChanged", (info, eventName) => {
         this.addClassName(temp, sel, info);
@@ -55,9 +65,11 @@ export default class TinySlider {
       
       
       
-    if(sliderId) {
-        window.tnsSliders[sliderId] = temp;
-      }
+    if(typeof(el.id) != "undefined") {
+        el.tnsSlider = temp;
+    }
+
+
     });
   }
 }
